840b8c158bfea8a6033d8977508096a42a7a20a1
Stacy Cashmore <33732891+StacyCash@users.noreply.github.com>
2024-01-14 21:41:07 +0100

    Remake (#2)
    Removéexisting apps, remake as .NET 8 and move original code back in.
    
    Also, debug deploy pipeline until working...

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,24 +1,27 @@
+using Api.Models;
+using CosmosDBAccessor;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using CosmosDBAccessor;
-using Api.Models;
 
 var environmentVariables = Environment.GetEnvironmentVariables();
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices(s =>
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
         {
-            s.AddSingleton<IContainerAccess<CosmosTodo>>(factory => new CosmosContainerAccess<CosmosTodo>(
+            services.AddSingleton<IContainerAccess<CosmosTodo>>(factory => new CosmosContainerAccess<CosmosTodo>(
                 new CosmosEndpointKeySettings(
                     (string)environmentVariables["CosmosEndpoint"]!,
                     (string)environmentVariables["CosmosKey"]!,
                     (string)environmentVariables["TodoDatabaseName"]!,
                     (string)environmentVariables["TodoContainerName"]!
-                    )));
-            s.AddSingleton<TodoHandler>();
+                )));
+            services.AddSingleton<TodoHandler>();
         }
-    )
+    })
     .Build();
 
 host.Run();
