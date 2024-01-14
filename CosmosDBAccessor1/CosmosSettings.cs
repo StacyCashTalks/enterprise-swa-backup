a63a407d32c6db5068dda0c7dd3e27fc917db925
Stacy Cashmore <33732891+StacyCash@users.noreply.github.com>
2024-01-14 14:45:55 +0100

    Fix folder name

--- CosmosDBAccessor1/CosmosSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CosmosDBAccessor;
-
-public record CosmosEndpointKeySettings(string Endpoint, string Key, string DatabaseName, string ContainerName);
-
-public record CosmosConnectionStringSettings(string ConnectionString, string DatabaseName, string ContainerName);
-
-public record CosmosDefaultCredentialSettings(string Endpoint, string DatabaseName, string ContainerName);
